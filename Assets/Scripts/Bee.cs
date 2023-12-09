ceec08870ae2bf1e6ace11a6e9b3c129b907d6f3
Andrew Kim <andrewhkim3@gmail.com>
2023-12-09 04:26:43 -0600

    Fix new borders for first game

--- Assets/Scripts/Bee.cs
+++ Assets/Scripts/Bee.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Bee : MonoBehaviour
 {
-    public GameObject border; // Reference to the Border GameObject
+    // public GameObject border; // Reference to the Border GameObject
+    public GameObject levelOneImage; // Reference to the Level One Image GameObject
     public int lives = 3;
-    
+
     private float delay;
     private Game g;
     private Vector3 curPos;
-    private bool colliding = false;
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +23,7 @@ public class Bee : MonoBehaviour
     {
         if(g.GetRespawn())
         {
-            Invoke("FollowMouse", 1);
+            Invoke("FollowMouse", 1); // Wait 1 second before following mouse
         }
         else if (g.beeCanMove)
         {
@@ -39,26 +39,46 @@ public class Bee : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         // Check if mouse collides with Border's collider
-        // if (border.GetComponent<BoxCollider2D>().OverlapPoint(mousePosition))
-        if(!colliding)
+        if (levelOneImage.GetComponent<PolygonCollider2D>().OverlapPoint(mousePosition))
+        // if(!colliding)
         {
             // Set bee position to mouse position, with Z-coordinate of 0
             transform.position = new Vector3(mousePosition.x, mousePosition.y, 0f);
             curPos = transform.position;
         }
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        colliding = true;
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if(curPos.x > Camera.main.ScreenToWorldPoint(Input.mousePosition))
+        else
         {
-            colliding = false;
+            // Don't move bee
+            transform.position = transform.position;
         }
-        
     }
+
+    // void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     colliding = false;
+    // }
+
+    // void OnCollisionExit2D(Collision2D collision)
+    // {
+    //     colliding = true;
+    // }
+
+
+    // {
+    //     // if(curPos.x > Camera.main.ScreenToWorldPoint(Input.mousePosition))
+    //     // {
+    //     //     colliding = false;
+    //     // }
+
+    //     // If bee is within the border, allow it to move
+    //     if (border.GetComponent<BoxCollider2D>().OverlapPoint(curPos))
+    //     {
+    //         colliding = false;
+    //     }
+    //     else
+    //     {
+    //         colliding = true;
+    //     }
+
+    // }
 }
